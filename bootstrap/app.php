0fef199179b975a1f74158206043fc4d7b752cf1
Ussa <brostk962@gmail.com>
2026-06-25 22:06:34 +0700

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,14 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (\Symfony\Component\HttpKernel\Exception\NotFoundHttpException $e, $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Record not found.'
+                ], 404)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            }
+        });
     })
     ->create();
